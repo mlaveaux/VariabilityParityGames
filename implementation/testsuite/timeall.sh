b237e534d0edc8fe186845f8a7780ff425bac7a1
sjef <sjefvanloo@gmail.com>
2019-06-20 16:34:52 +0200

    time script

--- implementation/testsuite/timeall.sh
+++ implementation/testsuite/timeall.sh
@@ -1,9 +1,15 @@
+echo "Time with bdd"
 ./time.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkBDD/ > $1/logs/time_bdd.log
+echo "Attr with bdd"
 ./attrtime.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkBDD/ > $1/logs/attrtime_bdd.log
 
+echo "Time with single"
 ./time_single.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkBDD/ > $1/logs/time_single.log
+echo "Attr with single"
 ./attrtime_single.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkBDD/ > $1/logs/attrtime_single.log
 
+echo "Time with explicit"
 ./time.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkExplicit/ > $1/logs/time_explicit.log
-./time.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkExplicit/ > $1/logs/time_explicit.log
+echo "Attr with explicit"
+./attrtime.sh $1 /home/sjef/AfstudeerRepo/implementation/testsuite/algs/ZlnkExplicit/ > $1/logs/attrtime_explicit.log
 
