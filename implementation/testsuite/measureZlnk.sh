733fcbd31b7418430c1db2d658bc12478b87a4df
sjef <sjefvanloo@gmail.com>
2019-08-05 16:41:38 +0200

    Measure zlnk

--- implementation/testsuite/measureZlnk.sh
+++ implementation/testsuite/measureZlnk.sh
@@ -1,22 +1,16 @@
 PROBLEM=$1
 CMD=$2
-DIR=$3
 
 cd $PROBLEM
 for g in game*
 do
         cd $g
-	mkdir -p $DIR/$g/
-        $CMD SVPG m$DIR/$g/ F > ../logs/${g}.log
+        $CMD SVPG m > ../logs/measurezlnk_${g}.log
 	echo -n "$g: "
-	S=`cat ../logs/${g}.log|grep "Assisted leaf"|awk '{ print $6 }'|tr '\n' '+'`
-	S=`echo ${S}0|bc`
-	echo -n $S,
-	echo -n `cat ../logs/${g}.log|grep "Assisted leaf"|awk 'BEGIN {total=0}
-{total+=$3}
-END {printf("%.2f\n",total/NR)}'`","
-	echo -n `head -n2 SVPG |tail -n1|awk '{print $2}'|sed 's/;//'`,
-	cat ../logs/${g}.log|grep Solved|grep games|awk '{ print $2}'
+	N=`cat ../logs/measurezlnk_${g}.log|grep -i attracted|wc -l`
+	sum=`cat ../logs/measurezlnk_${g}.log|grep -i attracted|awk '{ print $2 }'|tr '\n' '+'`
+	S=`echo ${sum}0|bc`
+	echo $N,$S
         cd ..
 done
 cd ..
