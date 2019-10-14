de4c23180c593a617507be82cd74cf2762cb96d9
sjef <sjefvanloo@gmail.com>
2019-10-14 14:52:11 +0200

    changed randomgames and randomscalegames

--- implementation/testsuite/randomscalegames/createFFgames_scaleconfs.sh
+++ implementation/testsuite/randomscalegames/createFFgames_scaleconfs.sh
@@ -2,16 +2,19 @@
 lambda=0.92
 for sub in a b c d
 do
-	for i in `seq 13 20`
+	for i in `seq 2 20`
 	do
-		N=$(((RANDOM%400)+10))
+		N=$(((RANDOM%500)+100))
 		P=$(((RANDOM%10)+1))
 		l=1
-		h=$(((RANDOM%($N/4))+1))
-		c=$i
-	
+		h=$(((RANDOM%(8))+3))
+		c=$i	
+
+		rm -f `pwd`/game_scaleconfs_$i$sub/*
+		rmdir `pwd`/game_scaleconfs_$i$sub
 		mkdir `pwd`/game_scaleconfs_$i$sub
 		echo "Create game with $N nodes, maxprio: $P, lowest out degree: $l, subhighest out degree: $h, 2^$c confs and lambda: $lambda with type FF in `pwd`/game_scaleconfs_$i$sub"
 		java -jar ../housekeeping.jar randomgame $N $P $l $h $c $lambda FF `pwd`/game_scaleconfs_$i$sub
 	done
 done
+for f in game*;do VPGSolver_bdd $f/SVPG Q$f;done
