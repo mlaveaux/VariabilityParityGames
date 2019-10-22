d837e5236cc873a30ab7a9876c001ec09d4745f3
sjef <s.v.loo@student.tue.nl>
2019-10-22 15:59:04 +0200

    Changed results and random games

--- implementation/testsuite/runatnight.sh
+++ implementation/testsuite/runatnight.sh
@@ -1,4 +1,9 @@
 echo "Collect singles"
-data/singles/collect.sh randomscalegames
+data/singles/collect.sh  randomgames
+echo "Collect fam"
+data/family/collect.sh randomgames
+echo "Collect singles"
+data/singles/collect.sh  randomscalegames
 echo "Collect fam"
 data/family/collect.sh randomscalegames
+
