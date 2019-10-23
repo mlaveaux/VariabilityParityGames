3644dc2ddc0050a69b37daaa0b90cfc67e209a71
sjef <sjefvanloo@gmail.com>
2019-10-23 17:44:14 +0200

    Report: section experimental evaluation

--- Report/results/localize.sh
+++ Report/results/localize.sh
@@ -6,10 +6,10 @@ while read -r r ; do
 	pattern=`echo $r|cut -d'-' -f2`
 	sed -i "s/$color/$pattern/g" "$working"
 done < replace
-echo '<svg class="main-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1000" height="500" viewBox="0 0 1000 500">' > "$FILE"
+echo '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="main-svg"  width="315" height="290" viewBox="55 85 370 365">' > "$FILE"
 cat defs >> "$FILE"
 cat "$working" |cut -d'>' --complement -f1 >> "$FILE"
 rm -f "$working"
 sed -i 's/sans-serif/arial/g' "$FILE"
 sed -i "s/'Open Sans'/arial/g" "$FILE"
-inkscape -z -e "$FILE.png" -w 1000 -h 500 "$FILE"
+inkscape -z -e "$FILE.png" -w 315 -h 290 "$FILE"
