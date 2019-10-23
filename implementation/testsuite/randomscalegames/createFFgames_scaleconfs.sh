6035193b0677ee9565a80220fa0254f108cc49d9
sjef <sjefvanloo@gmail.com>
2019-10-23 11:03:05 +0200

    new scalegames

--- implementation/testsuite/randomscalegames/createFFgames_scaleconfs.sh
+++ implementation/testsuite/randomscalegames/createFFgames_scaleconfs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 lambda=0.92
-for sub in a b c d
-do
-	for i in `seq 2 15`
+	for i in `seq 2 18`
 	do
 		N=$(((RANDOM%500)+100))
 		P=$(((RANDOM%10)+1))
@@ -16,4 +14,3 @@ do
 		echo "Create game with $N nodes, maxprio: $P, lowest out degree: $l, subhighest out degree: $h, 2^$c confs and lambda: $lambda with type FF in `pwd`/game_scaleconfs_$i$sub"
 		java -jar ../housekeeping.jar randomgame $N $P $l $h $c $lambda FF `pwd`/game_scaleconfs_$i$sub
 	done
-done
