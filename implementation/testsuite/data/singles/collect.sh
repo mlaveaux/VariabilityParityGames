8908d1fda4dd76b8f054e3721088926c4ce7cfdc
sjef <s.v.loo@student.tue.nl>
2019-10-25 15:03:09 +0200

    Data results using new elevator and modified fixed-point

--- implementation/testsuite/data/singles/collect.sh
+++ implementation/testsuite/data/singles/collect.sh
@@ -6,10 +6,10 @@ else
 fi
 for p in $probs
 do
-        echo Single Zlnk $p
-        ./time_single.sh $p VPGSolver_bdd_single > `pwd`/data/singles/${p}_zlnk.data
-	echo Single Zlnk local $p
-        ./time_single.sh $p VPGSolver_bdd_single l > `pwd`/data/singles/${p}_zlnk_local.data
+       # echo Single Zlnk $p
+        #./time_single.sh $p VPGSolver_bdd_single > `pwd`/data/singles/${p}_zlnk.data
+	#echo Single Zlnk local $p
+        #./time_single.sh $p VPGSolver_bdd_single l > `pwd`/data/singles/${p}_zlnk_local.data
 	echo Single MBR $p
 	./time_single.sh $p VPGSolver_bdd_single F > `pwd`/data/singles/${p}_MBR.data
 	echo Single MBR local $p
