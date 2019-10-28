21df59e2133325d8c44b91c0199eea111d32bc87
sjef <s.v.loo@student.tue.nl>
2019-10-28 09:48:36 +0100

    New results

--- implementation/testsuite/data/family/collect.sh
+++ implementation/testsuite/data/family/collect.sh
@@ -6,14 +6,14 @@ else
 fi
 for p in $probs
 do
-#	echo Family zlnk explicit $p
-#	./time.sh $p VPGSolver_explicit > `pwd`/data/family/${p}_zlnk_explicit.data
- #       echo Family zlnk explicit local $p
-  #      ./time.sh $p VPGSolver_explicit l > `pwd`/data/family/${p}_zlnk_explicit_local.data
-#	echo Family zlnk $p
- #       ./time.sh $p VPGSolver_bdd > `pwd`/data/family/${p}_zlnk.data
-  #      echo Family zlnk local $p
-   #   ./time.sh $p VPGSolver_bdd l > `pwd`/data/family/${p}_zlnk_local.data
+	echo Family zlnk explicit $p
+	./time.sh $p VPGSolver_explicit > `pwd`/data/family/${p}_zlnk_explicit.data
+        echo Family zlnk explicit local $p
+        ./time.sh $p VPGSolver_explicit l > `pwd`/data/family/${p}_zlnk_explicit_local.data
+	echo Family zlnk $p
+        ./time.sh $p VPGSolver_bdd > `pwd`/data/family/${p}_zlnk.data
+        echo Family zlnk local $p
+       ./time.sh $p VPGSolver_bdd l > `pwd`/data/family/${p}_zlnk_local.data
 	echo Family MBR $p
 	./time.sh $p VPGSolver_bdd F > `pwd`/data/family/${p}_MBR.data
 	echo Family MBR local $p
