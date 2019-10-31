da6e36fd35f5999406323bc3e746c1d89742d9bc
sjef <s.v.loo@student.tue.nl>
2019-10-31 15:46:47 +0100

    Results MBR measure

--- implementation/testsuite/data/measures/collect.sh
+++ implementation/testsuite/data/measures/collect.sh
@@ -8,4 +8,6 @@ for p in $probs
 do
 	echo measureZlnk $p
 	./measureZlnk.sh  $p VPGSolver_bdd > `pwd`/data/measures/${p}_zlnk.data
+	echo measureMBR $p
+	./measureMBR.sh $p VPGSolver_bdd l > `pwd`/data/measures/${p}_MBR_local.data
 done
--- implementation/testsuite/measureMBR.sh
+++ implementation/testsuite/measureMBR.sh
@@ -1,22 +1,24 @@
 PROBLEM=$1
 CMD=$2
-DIR=$3
+PARAM=$3
 
 cd $PROBLEM
 for g in game*
 do
         cd $g
-	mkdir -p $DIR/$g/
-        $CMD SVPG m$DIR/$g/ F > ../logs/${g}.log
+        $CMD SVPG m F $PARAM> ../logs/${g}.log
 	echo -n "$g: "
-	S=`cat ../logs/${g}.log|grep "Assisted leaf"|awk '{ print $6 }'|tr '\n' '+'`
-	S=`echo ${S}0|bc`
-	echo -n $S,
-	echo -n `cat ../logs/${g}.log|grep "Assisted leaf"|awk 'BEGIN {total=0}
-{total+=$3}
-END {printf("%.2f\n",total/NR)}'`","
-	echo -n `head -n2 SVPG |tail -n1|awk '{print $2}'|sed 's/;//'`,
-	cat ../logs/${g}.log|grep Solved|grep games|awk '{ print $2}'
+	S=$(echo `cat ../logs/${g}.log|grep Assisted|awk '{print $3}'|tr '\n' '+'`0|bc)
+	n=$(cat ../logs/${g}.log|grep Assisted|wc -l)
+	echo "$S,$n"
+#	S=`cat ../logs/${g}.log|grep "Assisted leaf"|awk '{ print $6 }'|tr '\n' '+'`
+#	S=`echo ${S}0|bc`
+#	echo -n $S,
+#	echo -n `cat ../logs/${g}.log|grep "Assisted leaf"|awk 'BEGIN {total=0}
+#{total+=$3}
+#END {printf("%.2f\n",total/NR)}'`","
+#	echo -n `head -n2 SVPG |tail -n1|awk '{print $2}'|sed 's/;//'`,
+#	cat ../logs/${g}.log|grep Solved|grep games|awk '{ print $2}'
         cd ..
 done
 cd ..
